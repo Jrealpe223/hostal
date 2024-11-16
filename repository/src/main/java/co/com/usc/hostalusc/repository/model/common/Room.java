3f98c7163ad84133d3a05c03716812d1a88950c1
kitotsu-molina <jhojan.e.m.v.2018@gmail.com>
2024-11-16 01:40:47 -0500

    Se agrego api de busqueda de room por fecha y filtro general solucion bug #2

--- repository/src/main/java/co/com/usc/hostalusc/repository/model/common/Room.java
+++ repository/src/main/java/co/com/usc/hostalusc/repository/model/common/Room.java
@@ -26,6 +26,9 @@ public class Room {
     @Column(name = "room_number", unique = true, nullable = false)
     private String roomNumber;
 
+    @Column(name = "description")
+    private String description;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "room_type", length = 50, nullable = false)
     private RoomTypeEnum roomType;
@@ -38,6 +41,18 @@ public class Room {
 
     @Column(name = "dormitories")
     private Integer dormitories;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "bedrooms")
+    private Integer bedrooms;
+
+    @Column(name = "image")
+    private String image;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status", length = 50, nullable = false) // e.g., Available, Reserved, Maintenance
